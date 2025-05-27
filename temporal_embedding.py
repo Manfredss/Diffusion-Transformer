dbbcc925f303ca319b1dcc58e3f7b4ff998d6574
Wenfei Qi <wenfeiqi@usc.edu>
2025-05-27 21:14:11 +0800

    implement time embedding

---
+++ temporal_embedding.py
@@ -0,0 +1,28 @@
+from torch import nn
+from config import T
+import torch
+import math
+
+
+class TimeEmbedding(nn.Module):
+    def __init__(self, embSize: int):
+        super().__init__()
+        self.halfDim = embSize // 2
+        halfEmb = torch.exp(torch.arange(self.halfDim, dtype=torch.float32) * (-math.log(10000) / self.halfDim - 1))
+        self.register_buffer('halfEmb', halfEmb)
+
+    def forward(self, x):
+        x = x.view(x.size(0), 1)
+        halfEmb = self.halfEmb.unsqueeze(0).expand(x.size(0), self.halfDim)
+        halfEmbX = halfEmb * x
+        embX = torch.cat((torch.sin(halfEmbX), torch.cos(halfEmbX)), dim=-1)
+        return embX
+    
+
+if __name__ == '__main__':
+    timeEmb = TimeEmbedding(16)
+    x = torch.randint(0, T, (2,))
+    embs = timeEmb(x)
+    print(embs)
+
+
